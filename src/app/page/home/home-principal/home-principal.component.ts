7543b7e60228daccc6c8af4873585232ada990e6
Raissa Peneluc <raissapeneluc@gmail.com>
2023-11-23 15:54:46 -0300

    Atualizações

--- src/app/page/home/home-principal/home-principal.component.ts
+++ src/app/page/home/home-principal/home-principal.component.ts
@@ -9,11 +9,6 @@ export class HomePrincipalComponent implements OnInit {
 
   constructor() { }
 
-  clickNoStart() {
-    console.log('Botão clicado!');
-    // Adicione a lógica desejada aqui, por exemplo, redirecionar para outra página ou executar alguma ação.
-  }
-
   ngOnInit(): void {
   }
 
